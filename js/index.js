5a3ca95bc22b183d8c0156ed1974519049c7f313
Julio Bermudez <jbermudez@distritomoda.com.ar>
2021-09-03 12:52:26 -0300

    asd

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ document.querySelector("#search").addEventListener("click", function (e) {
   document.querySelector("#data").innerHTML = `<h4> - Loading...</h4>`;
   fetch("http://localhost:4550/api/buscar_pedido", {
     body: JSON.stringify({
-      n_pedido: document.querySelector("#label-barcode").value,
+      n_pedido: document.querySelector("#label-barcode").value.split('0')[1],
     }),
     method: "POST",
     headers: {
@@ -40,7 +40,7 @@ document.querySelector("#search").addEventListener("click", function (e) {
       console.log(data);
       PrintData(data, document.querySelector("#data"));
     })
-    .catch((error) => console.warn(error.message));
+    .catch((error) => console.warn(error.message));    
 });
 /**traer data de la api */
 /**funcion para imprimir la data */
@@ -49,8 +49,11 @@ function PrintData(response, element) {
     let orders = response.data;
     for (let i = 0; i < orders.length; i++) {
       let tpl = `
-                        <p>UT_pedido: ${orders[i].UT_pedido}</p>
-                        <p>direccion_ciudad_pedido: ${orders[i].direccion_ciudad_pedido}</p>
+                        <p>DNI: ${orders[i]['DNI']}</p>
+                        <p>NOMBRE: ${orders[i]['NOMBRE']}</p>
+                        <p>MAIL: ${orders[i]['MAIL']}</p>
+                        <p>PROVINCIA: ${orders[i]['PROVINCIA']}</p>
+                        <p>DIRECCION: ${orders[i]['DIRECCION']}</p>
                     `;
       element.innerHTML = tpl;
     }
